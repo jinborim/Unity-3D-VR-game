31c0666412c363b935da19261bcfade99a0606ec
jinborim <121596050+jinborim@users.noreply.github.com>
2024-05-29 01:53:39 +0900

    Create decompositionable.cs

---
+++ Bathroom1/decompositionable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decompositionable : MonoBehaviour
+{
+    public GameObject[] decomposition_result_Item;
+}
