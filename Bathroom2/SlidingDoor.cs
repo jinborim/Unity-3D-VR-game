85166bedcc1e20f541971dab2939056aeeef46f6
jinborim <121596050+jinborim@users.noreply.github.com>
2024-05-28 22:06:23 +0900

    Create SlidingDoor.cs

---
+++ Bathroom2/SlidingDoor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NavKeypad
+{
+    public class SlidingDoor : MonoBehaviour
+    {
+        [SerializeField] private Animator anim;
+        public bool IsOpoen => isOpen;
+        private bool isOpen = false;
+
+        public void ToggleDoor()
+        {
+            isOpen = !isOpen;
+            anim.SetBool("isOpen", isOpen);
+        }
+
+        public void OpenDoor()
+        {
+            isOpen = true;
+            anim.SetBool("isOpen", isOpen);
+        }
+        public void CloseDoor()
+        {
+            isOpen = false;
+            anim.SetBool("isOpen", isOpen);
+        }
+
+        public void C()
+        {
+            isOpen = !isOpen;
+            anim.SetBool("isOpen", isOpen);
+        }
+    }
+}
