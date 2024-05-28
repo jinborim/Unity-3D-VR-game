87ba49436734e15706d54b4ff52f852c8c1041cc
jinborim <121596050+jinborim@users.noreply.github.com>
2024-05-29 00:17:58 +0900

    Create Combinationable.cs

---
+++ Bathroom1/Combinationable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combinationable : MonoBehaviour
+{
+    public GameObject[] combinationableItem;
+
+}
