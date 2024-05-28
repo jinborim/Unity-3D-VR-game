9749ed84ad211080a569d8d24a6ac98a96ed815e
jinborim <121596050+jinborim@users.noreply.github.com>
2024-05-28 22:47:15 +0900

    Create ITEM_LIST.cs

---
+++ HallWay/BackgroundManager/ITEM_LIST.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ITEM_LIST : MonoBehaviour
+{
+    //조합으로 만들 수 있는 아이템만!!!!
+    public Item[] items;
+
+}
